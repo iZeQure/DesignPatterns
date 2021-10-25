9f81317706a0b22212f46a3e411aff3d12560d60
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-10-25 13:13:55 +0200

    Project Init
    Created a base project with a console application as the UI.

---
+++ DesignPatternsDemo/UIApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UIApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
